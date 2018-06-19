51b012368080fb861242113a6177bad288b75468
Kei Sugano <ks@polyswarm.io>
2018-06-19 15:22:33 +0900

    geth error fix

--- scripts/migrate_and_create_config.sh
+++ scripts/migrate_and_create_config.sh
@@ -2,18 +2,18 @@
 
 say(){
     echo "============================================="
-    echo $1
+    echo $(IFS="\n" ; echo "$@")
     echo "============================================="
 }
 
-cur_gas_limit(){
-    limit=$(truffle migrate --reset)
-    say $limit
-    sleep 2
-    cur_gas_limit
-}
+# cur_gas_limit(){
+#     limit=$(truffle migrate --reset)
+#     say $limit
+#     cur_gas_limit
+# }
+# 
+# sleep 10
+# cur_gas_limit
 
-cur_gas_limit
-
-# truffle migrate --reset
-# truffle exec scripts/create_config.js --home=http://geth:8545 --side=http://sidechain:8545 --ipfs=http://ipfs:4001
+truffle migrate --reset
+truffle exec scripts/create_config.js --home=http://geth:8545 --side=http://sidechain:8545 --ipfs=http://ipfs:4001
