4deb694a72092a80e6d65394527a248c3d70130d
Kei Sugano <ks@polyswarm.io>
2018-06-19 16:48:15 +0900

    XXX

--- scripts/migrate_and_create_config.sh
+++ scripts/migrate_and_create_config.sh
@@ -9,7 +9,7 @@ say(){
 progress(){
     END=60
     for i in $(seq 1 $END); do
-        if [ $i % 3 -eq 0 ]; then
+        if [ $i % 3 == 0 ]; then
           echo "/"
         #elif [ $i % 3 -eq 1 ]; then
         #  echo "-"
